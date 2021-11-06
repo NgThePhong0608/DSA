1d018a6495d392a09710a29c74468057664b57ff
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2021-11-06 07:16:22 +0700

    DSA

--- Linkedlist/lab1.cpp
+++ Linkedlist/lab1.cpp
@@ -1,63 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
     Node *pNext;
 };
-typedef struct Node* node;
+typedef struct Node *node;
 
 // cap phat dong 1 not voi du lieu la so nguye nx
-node makeNode(int x){
+node makeNode(int x)
+{
     node tmp = new Node();
     tmp->data = x;
     tmp->pNext = NULL;
     return tmp;
 }
 
-bool isEmpty(node a){
-    if(a != NULL){
+bool isEmpty(node a)
+{
+    if (a != NULL)
+    {
         return false;
     }
     return true;
 }
 
-int size(node a){
+int size(node a)
+{
     int count = 0;
-    while(a != NULL){
+    while (a != NULL)
+    {
         a = a->pNext; // gan dia chi cua node tiep theo cho node hien tai
         ++count;
     }
     return count;
 }
 
-void insertFirst(node &a, int x){
+void insertFirst(node &a, int x)
+{
     node temp = makeNode(x);
     if (a == NULL)
     {
         a = temp;
     }
-    else{
+    else
+    {
         temp->pNext = a;
         a = temp;
     }
 }
 
-void insertLast(node &a, int x){
+void insertLast(node &a, int x)
+{
     node temp = makeNode(x);
     if (a == NULL)
     {
         a = temp;
-    }else{
+    }
+    else
+    {
         node p = a;
-        while(p->pNext != NULL){
+        while (p->pNext != NULL)
+        {
             p = p->pNext;
         }
         p->pNext = temp;
     }
 }
 
-void insertMiddle(node &a, int x, int pos){
+void insertMiddle(node &a, int x, int pos)
+{
     int n = size(a);
     if (pos <= 0 || pos > pos + 1)
     {
@@ -65,16 +78,17 @@ void insertMiddle(node &a, int x, int pos){
     }
     if (n == 1)
     {
-        insertFirst(a,x);
+        insertFirst(a, x);
         return;
     }
     else if (n == pos + 1)
     {
-        insertLast(a,x);
+        insertLast(a, x);
         return;
     }
     node p = a;
-    for(int i = 1; i < pos - 1 ; i++){
+    for (int i = 1; i < pos - 1; i++)
+    {
         p = p->pNext;
     }
     node temp = makeNode(x);
@@ -82,8 +96,8 @@ void insertMiddle(node &a, int x, int pos){
     p->pNext = temp;
 }
 
-
-void deleteFirst(node &a){
+void deleteFirst(node &a)
+{
     if (a == NULL)
     {
         return;
@@ -91,27 +105,32 @@ void deleteFirst(node &a){
     a = a->pNext;
 }
 
-void deleteLast(node &a){
+void deleteLast(node &a)
+{
     if (a == NULL)
     {
         return;
     }
     node trc = NULL;
     node sau = a;
-    while(sau->pNext != NULL) {
+    while (sau->pNext != NULL)
+    {
         trc = sau;
         sau = sau->pNext;
     }
     if (trc == NULL)
     {
         a = NULL;
-    }else{
+    }
+    else
+    {
         trc->pNext = NULL;
     }
 }
 
-void deleteMiddle(node &a, int pos){
-    if (pos <= 0 || pos > size(a))  
+void deleteMiddle(node &a, int pos)
+{
+    if (pos <= 0 || pos > size(a))
     {
         return;
     }
@@ -124,60 +143,82 @@ void deleteMiddle(node &a, int pos){
     if (truoc == NULL)
     {
         a = a->pNext;
-    }else{
+    }
+    else
+    {
         truoc->pNext = sau->pNext;
     }
 }
 
-
-void output(node a){
-    cout<<"--------------------------\n";
-    while(a != NULL) {
-        cout<< a->data << "  ";
-        a=a->pNext;
+void output(node a)
+{
+    cout << "--------------------------\n";
+    while (a != NULL)
+    {
+        cout << a->data << "  ";
+        a = a->pNext;
     }
-    cout<<"\n--------------------------\n";
+    cout << "\n--------------------------\n";
 }
 
 int main()
 {
     node head = NULL;
-    while(1){
-        cout<<"-----------MENU-------------\n";
-        cout<<"1. Chen phan tu vao dau"<<endl;
-        cout<<"2. Chen phan tu vao cuoi"<<endl;
-        cout<<"3. Chen phan tu vao giua"<<endl;
-        cout<<"4. Xoa phan tu dau"<<endl;
-        cout<<"5. Xoa phan tu cuoi"<<endl;
-        cout<<"6. Xoa phan tu giua"<<endl;
-        cout<<"7. Duyet danh sach lien ket"<<endl;
-        cout<<"-----------------------------\n";
-        cout<<"Nhap lua chon: ";
-        int lc; cin>>lc;
-        if (lc == 1){
+    while (1)
+    {
+        cout << "-----------MENU-------------\n";
+        cout << "1. Chen phan tu vao dau" << endl;
+        cout << "2. Chen phan tu vao cuoi" << endl;
+        cout << "3. Chen phan tu vao giua" << endl;
+        cout << "4. Xoa phan tu dau" << endl;
+        cout << "5. Xoa phan tu cuoi" << endl;
+        cout << "6. Xoa phan tu giua" << endl;
+        cout << "7. Duyet danh sach lien ket" << endl;
+        cout << "-----------------------------\n";
+        cout << "Nhap lua chon: ";
+        int lc;
+        cin >> lc;
+        if (lc == 1)
+        {
             int x;
-            cout<<"Nhap gia tri can chen: ";
-            cin>>x;
-            insertFirst(head,x);
-        }else if (lc == 2){
+            cout << "Nhap gia tri can chen: ";
+            cin >> x;
+            insertFirst(head, x);
+        }
+        else if (lc == 2)
+        {
             int x;
-            cout<<"Nhap gia tri can chen: ";
-            cin>>x;
-            insertLast(head,x);
-        }else if (lc == 3){
-            int x;cout<<"Nhap gia tri can chen: ";cin>>x;
-            int pos;cout<<"Nhap vi tri: ";cin>>pos;
-            insertMiddle(head,x,pos);
-        }else if (lc == 4){
+            cout << "Nhap gia tri can chen: ";
+            cin >> x;
+            insertLast(head, x);
+        }
+        else if (lc == 3)
+        {
+            int x;
+            cout << "Nhap gia tri can chen: ";
+            cin >> x;
+            int pos;
+            cout << "Nhap vi tri: ";
+            cin >> pos;
+            insertMiddle(head, x, pos);
+        }
+        else if (lc == 4)
+        {
             deleteFirst(head);
-        }else if (lc == 5){
+        }
+        else if (lc == 5)
+        {
             deleteLast(head);
-        }else if (lc == 6){
+        }
+        else if (lc == 6)
+        {
             int x;
-            cout<<"Nhap vi tri can xoa: ";
-            cin>>x;
-            deleteMiddle(head,x);
-        }else{
+            cout << "Nhap vi tri can xoa: ";
+            cin >> x;
+            deleteMiddle(head, x);
+        }
+        else
+        {
             output(head);
         }
     }
